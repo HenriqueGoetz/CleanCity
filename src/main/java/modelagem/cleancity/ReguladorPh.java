4f2a1a29fe7136ebb959e03320ec9acfa1105a51
mabergmann <32148891+mabergmann@users.noreply.github.com>
2018-05-28 15:38:23 -0300

    sensorPh and reguladorPh implemented but not tested.

--- src/main/java/modelagem/cleancity/ReguladorPh.java
+++ src/main/java/modelagem/cleancity/ReguladorPh.java
@@ -5,11 +5,40 @@ package modelagem.cleancity;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Henrique Goetz
  */
 public class ReguladorPh {
+
+    private SensorPh sensorPh = new SensorPh();
+    private Coordenada coord;
+
+    public ReguladorPh(double lati, double longi) {
+        this.coord.setLatitude(lati);
+        this.coord.setLongitude(longi);
+    }
     
+    private void elevarPH(){
+        System.out.println("O ph está sendo elevado");
+    }
+    
+    private void reduzirPH(){
+        System.out.println("O ph está sendo elevado");
+    }
+    
+    // TODO: Verificar RN para nível onde eleva/reduz o PH e nível para notificar
+    public void testarPH(){
+        float phAgora = sensorPh.getLeituraPh();
+        if(phAgora>8){
+            this.elevarPH();
+        }else if(phAgora < 6){
+            this.reduzirPH();
+        }
+        if(phAgora>9 || phAgora <5){
+            // TODO: Verificar como será feita a notificação
+            System.out.print("Notificando prefeitura. Ph em: " + Float.toString(phAgora));
+        }
+    }
+
 }
--- src/main/java/modelagem/cleancity/SensorPh.java
+++ src/main/java/modelagem/cleancity/SensorPh.java
@@ -1,26 +1,38 @@
 package modelagem.cleancity;
 
+import java.util.Random;
+
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Henrique Goetz
  */
 public class SensorPh extends Sensor {
-    
-    private float leituraPh;
+
+    private float leituraPh = 7;
 
     public float getLeituraPh() {
+        this.lerPH();
         return leituraPh;
     }
 
     public void setLeituraPh(float leituraPh) {
         this.leituraPh = leituraPh;
     }
-    
+
+    private void lerPH() {
+        Random random = new Random();
+        setLeituraPh((float) (random.nextGaussian() + this.leituraPh));
+        if (this.leituraPh < 0) {
+            setLeituraPh(0);
+        } else if (this.leituraPh > 14) {
+            setLeituraPh(14);
+        }
+    }
 
 }
