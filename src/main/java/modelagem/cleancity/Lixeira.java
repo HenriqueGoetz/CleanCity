afb9b2790cba9b3e093aa06255faef2354ef161b
mabergmann <>
2018-05-28 14:33:07 -0300

    getPeso e getVolume added to class Lixeira

--- src/main/java/modelagem/cleancity/Lixeira.java
+++ src/main/java/modelagem/cleancity/Lixeira.java
@@ -14,13 +14,21 @@ public class Lixeira {
 
     private Capacidade capacidade;
     private Coordenada coord;
-    private SensorSonar sensorVolume;
-    private Balanca balanca;
+    private SensorSonar sensorVolume = new SensorSonar();
+    private Balanca balanca = new Balanca();
 
     public Lixeira(double lati, double longi) {
         this.capacidade.setPeso(0);
         this.capacidade.setVolume(0);
         this.coord.setLatitude(lati);
         this.coord.setLongitude(longi);
+    }
+    
+    public float getPeso(){
+        return balanca.getLeituraPeso();
+    }
+    
+    public float getVolume(){
+        return sensorVolume.getLeituraVolume();
     }
 }
